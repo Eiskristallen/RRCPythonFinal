a10082b668a2aa695b50281fe15e59f66c776b3d
Eiskristallen <qhuangb47@gmail.com>
2021-12-01 22:04:27 -0600

    feat:retrieve weather data for single page

--- scrape_weather.py
+++ scrape_weather.py
@@ -10,10 +10,26 @@ class WeatherScraper(HTMLParser):
     isA = False
     isTd = False
     td_count = 0
+    weather = {}
+    days = -1
+    year = 2018
+    month = 1
+    # year_month = 0;
+    if month < 10:
+        year_month = str(year)+'-'+'0'+str(month)
+    else:
+        year_month = str(year)+'-'+str(month)
+
+    def parse_data(self):
+        with urllib.request.urlopen('http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=1') as response:
+            html = str(response.read())
+        self.weather[str(self.year_month)] = {}
+        self.feed(html)
 
     def handle_starttag(self, tag, attrs):
         if bool(tag == 'tr'):
             self.isTr = True
+            self.days = self.days + 1
         # if tag == 'a':
         #     self.isA = True
         if bool(tag == 'td') & bool(self.td_count <= 2):
@@ -24,6 +40,7 @@ class WeatherScraper(HTMLParser):
         if bool(tag == 'tr'):
             self.isTr = False
             self.td_count = 0
+            # print('\n')
         if bool(tag == 'td'):
             self.isTd = False
         # if tag == 'a':
@@ -36,16 +53,29 @@ class WeatherScraper(HTMLParser):
             self.isTr = False
         if 'Xtrm' in data:
             self.isTr = False
-        if self.isTr & self.isTd & bool('LegendE' not in data) & bool('LegendM' not in data) & bool('E' not in data):
+        if self.isTr & self.isTd & bool('LegendE' not in data) & bool('LegendM' not in data):
             data = data.replace(r'\n', '').replace(
                 r'\r', '').replace(r'\t', '')
-            print(data)
-            # if self.isA:
-            #     print(data + 'wdasd')
+            if 'M' in data:
+                self.isTr = False
+                return
+            if 'E' in data:
+                self.isTr = False
+                return
+        if self.isTr & self.isTd & bool('LegendE' not in data) & bool('LegendM' not in data):
+            data = data.replace(r'\n', '').replace(
+                r'\r', '').replace(r'\t', '')
+            if self.td_count == 1:
+                self.weather[self.year_month][str(self.days)
+                                              ] = {'Max': float(data)}
+            if self.td_count == 2:
+                self.weather[self.year_month][str(self.days)
+                                              ]['Min'] = float(data)
+            if self.td_count == 3:
+                self.weather[self.year_month][str(self.days)
+                                              ]['Mean'] = float(data)
 
 
 myparser = WeatherScraper()
-
-with urllib.request.urlopen('http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5') as response:
-    html = str(response.read())
-myparser.feed(html)
+myparser.parse_data()
+print(myparser.weather)
