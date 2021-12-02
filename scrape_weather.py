76e4de655b0ad9d8713958406013837bcdde787e
Eiskristallen <qhuangb47@gmail.com>
2021-12-01 22:51:50 -0600

    feat: retrieve multiple years data

--- scrape_weather.py
+++ scrape_weather.py
@@ -12,19 +12,27 @@ class WeatherScraper(HTMLParser):
     td_count = 0
     weather = {}
     days = -1
-    year = 2018
-    month = 1
-    # year_month = 0;
-    if month < 10:
-        year_month = str(year)+'-'+'0'+str(month)
-    else:
-        year_month = str(year)+'-'+str(month)
+    year = 2021
+    month = 0
+    year_month = 0
 
     def parse_data(self):
-        with urllib.request.urlopen('http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=1') as response:
-            html = str(response.read())
-        self.weather[str(self.year_month)] = {}
-        self.feed(html)
+        for x in range(1, 10):
+            self.year = self.year - 1
+            self.month = 0
+            for x in range(1, 12):
+                self.month = self.month + 1
+                self.days = -1
+                with urllib.request.urlopen('http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year='+str(self.year)+'&Month=+'+str(self.month)) as response:
+                    html = str(response.read())
+                    if self.month < 10:
+                        self.year_month = str(
+                            self.year)+'-'+'0'+str(self.month)
+                    else:
+                        self.year_month = str(self.year)+'-'+str(self.month)
+                self.weather[str(self.year_month)] = {}
+                self.feed(html)
+        return self.weather
 
     def handle_starttag(self, tag, attrs):
         if bool(tag == 'tr'):
@@ -62,6 +70,8 @@ class WeatherScraper(HTMLParser):
             if 'E' in data:
                 self.isTr = False
                 return
+            if '\xa0' in data:
+                self.isTr = False
         if self.isTr & self.isTd & bool('LegendE' not in data) & bool('LegendM' not in data):
             data = data.replace(r'\n', '').replace(
                 r'\r', '').replace(r'\t', '')
@@ -77,5 +87,4 @@ class WeatherScraper(HTMLParser):
 
 
 myparser = WeatherScraper()
-myparser.parse_data()
-print(myparser.weather)
+print(myparser.parse_data())
